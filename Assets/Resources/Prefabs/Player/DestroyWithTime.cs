5aa994bdb41defa07f4864f5163ed1bdc02acdd2
LicenselessHunter <106037923+LicenselessHunter@users.noreply.github.com>
2022-05-27 03:04:27 -0400

    Add files via upload

---
+++ Assets/Resources/Prefabs/Player/DestroyWithTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWithTime : MonoBehaviour
+{
+	public float timer;
+
+    void Update()
+    {
+        StartCoroutine(DestroyOverTime());
+    }
+
+	IEnumerator DestroyOverTime()
+	{
+		yield return new WaitForSeconds(timer);
+		Destroy(gameObject);
+	}
+}
